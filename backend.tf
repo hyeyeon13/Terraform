e6957c5a73a9aefa5176ecf69eabe27f4a2ffd76
EC2 Default User <ec2-user@ip-10-0-10-192.ap-northeast-2.compute.internal>
2023-12-11 06:10:09 +0000

    please

--- backend.tf
+++ backend.tf
@@ -1,28 +1,3 @@
-provider "aws" {
-  region = "ap-northeast-2" # Please use the default region ID
-}
-
-# S3 버킷을 생성한다
-resource "aws_s3_bucket" "for_tfstate" {
-  bucket = "hy-terraform-tfstate"
-}
-# S3 버킷의 버저닝 기능 활성화 선언한다.
-resource "aws_s3_bucket_versioning" "tfstate" {
-  bucket = aws_s3_bucket.for_tfstate.bucket
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# DynamoDB for terraform state lock
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "terraform-lock"
-  hash_key     = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6367af428b52c46553d2f30b1691e2872b8b97992ee6b6a547715b24d5cf3422
+size 637
