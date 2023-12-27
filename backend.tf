a77a923bc3406665316863a40901163921123094
EC2 Default User <ec2-user@ip-10-0-10-192.ap-northeast-2.compute.internal>
2023-12-27 08:12:25 +0000

    change

--- backend.tf
+++ backend.tf
@@ -1,3 +1,39 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9f9f9f92c19cf42020e21ab677ae2837a57c418e4abde9aa3ab862e948e8b77f
-size 875
+# S3 버킷을 생성한다
+resource "aws_s3_bucket" "for_tfstate" {
+  bucket = "hy-terraform-tfstate-change-2"
+}
+# S3 버킷의 버저닝 기능 활성화 선언한다.
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.for_tfstate.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# DynamoDB for terraform state lock
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "terraform-lock"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+// Backend
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    region         = "ap-northeast-2"
+    # bucket         = "${var.project_name}-${var.account_name}-terraform-state"
+    # key            = "${var.project_name}-${var.account_name}-terraform.tfstate"
+    # dynamodb_table = "${var.project_name}_${var.account_name}_terraform_lock"
+    bucket         = "hy-terraform-tfstate-change-1"
+    key            = "hy-terraform.tfstate"
+    dynamodb_table = "hy_terraform_lock"
+    #encrypt = true
+  }
+} 
--- varaiables.tf
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b7b9395ac24c6de0fadda8e507fd8517463d1464f8e22ec7cf282e21c5a1fb67
-size 896
